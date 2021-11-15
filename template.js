719853d6f8ea4f4fc0d6ebadd83c52fc70d908ba
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-11-16 02:49:08 +0600

    template string

---
+++ template.js
@@ -0,0 +1,13 @@
+const firstName = "Rahim";
+const lastName = "Uddin";
+
+const fullName = `${firstName} ${lastName} is a good Man.`;
+
+const multiLine = `Hello!
+How are you?
+Where are you from?
+What is your name?`;
+
+console.log(fullName);
+
+console.log(multiLine);
